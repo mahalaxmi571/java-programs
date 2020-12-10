fd35f0aa8069db9ee01f6ee284f3b6e7de0d2344
mahalaxmi571 <75772427+mahalaxmi571@users.noreply.github.com>
2020-12-10 20:14:58 +0530

    post decrement

---
+++ PostDecrement.java
@@ -0,0 +1,8 @@
+class PostDecrement{
+  public static void main(String args[]){
+   int a=33;
+   int PostDec=a--;
+   System.out.println("the value of PostDec is:"+PostDec);
+   System.out.println("the value of a is:"+a);
+   }
+   }
