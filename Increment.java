2a60baffd891ffe0d09b6617c40a1dd481c751b3
mahalaxmi571 <75772427+mahalaxmi571@users.noreply.github.com>
2020-12-10 20:16:38 +0530

    Increment

---
+++ Increment.java
@@ -0,0 +1,8 @@
+class Increment{
+  public static void main(String args[]){
+  int a=15;
+  int PostInc=a++;
+  System.out.println("the value of PostInc is:"+PostInc);
+  System.out.println("the value of a is:"+a);
+  }
+  }
