dbf1920ff9b8e0b60a0c136be48a4ff997ff0a2e
mahalaxmi571 <75772427+mahalaxmi571@users.noreply.github.com>
2020-12-10 20:20:52 +0530

    if condition

---
+++ CheckPosNegExample.java
@@ -0,0 +1,10 @@
+class CheckPosNegExample{
+   public static void main(String args[]){
+    int b=5;
+	if(b>0){
+	 System.out.println("the value is positive");
+	} else {
+	 System.out.println("the value is negative");
+   }
+  }
+}
