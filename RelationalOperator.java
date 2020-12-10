be971ed7a26ac1b276d9a72c36bc37d8b269c02e
mahalaxmi571 <75772427+mahalaxmi571@users.noreply.github.com>
2020-12-10 20:18:30 +0530

    Relational operator

---
+++ RelationalOperator.java
@@ -0,0 +1,25 @@
+class RelationalOperator{
+  public static void main(String args[]){
+  int a=10;
+  int b=20;
+  
+  // == operaor
+  System.out.println("a==b is:"+(a==b));
+  
+  // <= operator  
+  System.out.println("a<=b is:"+(a<=b));  
+  
+   // >= operator 
+  System.out.println("a==b is:"+(a>=b));
+
+  
+  //!= operaor
+    System.out.println("a==b is:"+(a!=b)); 
+
+   // > operator
+	System.out.println("a==b is:"+(a>b));
+
+   //< operator
+    System.out.println("a==b is:"+(a<b));	
+}
+  }
