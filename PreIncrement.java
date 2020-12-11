22f33d598138d08cbfe3df61fb9f30486b92678b
mahalaxmi571 <75772427+mahalaxmi571@users.noreply.github.com>
2020-12-11 19:30:41 +0530

    preIncrement

---
+++ PreIncrement.java
@@ -0,0 +1,8 @@
+class PreIncrement{
+  public static void main(String args[]){
+   int a=10;
+   int PreInc=++a;
+   System.out.println("the value of PreInc is:"+PreInc);
+   System.out.println("the value of a is:"+a);
+   }
+   }
