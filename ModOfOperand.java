29e4e9abc25d351412c84276ec4bdd346546182f
mahalaxmi571 <75772427+mahalaxmi571@users.noreply.github.com>
2020-12-11 20:27:28 +0530

    ModulusOfOperand

---
+++ ModOfOperand.java
@@ -0,0 +1,9 @@
+class ModOfOperand{
+     public static void main(String args[]){
+	       int a=1;
+		   int b=2;
+		   int c=3;
+		   int Mod=a%b%c;
+	    System.out.println("the value of Mod is:" + Mod);
+		 }
+		 }
