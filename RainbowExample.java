34926e8b2a04051e20ab52dc22d9b1637bc103f8
mahalaxmi571 <75772427+mahalaxmi571@users.noreply.github.com>
2020-12-11 19:34:00 +0530

    switchExample

---
+++ RainbowExample.java
@@ -0,0 +1,34 @@
+
+public class RainbowExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("prg to illustrate rainbow color");
+		String colour = "red";
+		switch (colour) {
+		case "violet":
+			System.out.println("it is rainbow colour");
+			break;
+		case "green":
+			System.out.println("it is rainbow colour");
+			break;
+		case "blue":
+			System.out.println("it is rainbow colour");
+			break;
+		case "indigo":
+			System.out.println("it is rainbow colour");
+			break;
+		case "red":
+			System.out.println("it is rainboe colour");
+			break;
+		case "yellow":
+			System.out.println("it is rainbow colour");
+			break;
+		case "orange":
+			System.out.println("it is rainbow colour");
+			break;
+		default:
+			System.out.println("it is not rainbow colour");
+		}
+	}
+}
