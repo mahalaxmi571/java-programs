1b9b2ed361855b4ca75d73b6168098f473d5f418
mahalaxmi571 <75772427+mahalaxmi571@users.noreply.github.com>
2020-12-11 20:25:09 +0530

    MultiplicationOperator

---
+++ MultOfOperand.java
@@ -0,0 +1,9 @@
+class MultOfOperand{
+     public static void main(String args[]){
+	       int a=1;
+		   int b=2;
+		   int c=3;
+		   int Mult=a*b*c;
+	    System.out.println("the value of Mult is:"+Mult);
+		 }
+		 }
