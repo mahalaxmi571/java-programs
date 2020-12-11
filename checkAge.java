475823607dc41e125987b27233c33914f114fd4e
mahalaxmi571 <75772427+mahalaxmi571@users.noreply.github.com>
2020-12-11 19:28:49 +0530

    checkage

---
+++ checkAge.java
@@ -0,0 +1,10 @@
+class CheckAge{
+  public static void main(String args[]){
+    int a=17;
+	if(a>=18){
+	System.out.println("you are eligible to vote");
+	} else {
+	System.out.println("you are under age");
+	}
+  }
+  }
