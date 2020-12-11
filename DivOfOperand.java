241f74b3dfb71bb5878adccd4538a4124ebb6af4
mahalaxmi571 <75772427+mahalaxmi571@users.noreply.github.com>
2020-12-11 20:26:17 +0530

    DivOfOperand

---
+++ DivOfOperand.java
@@ -0,0 +1,9 @@
+class DivOfOperand{
+     public static void main(String args[]){
+	       int a=1;
+		   int b=2;
+		   int c=3;
+		   int Div=a/b/c;
+	    System.out.println("the value of Div is:"+Div);
+		 }
+		 }
