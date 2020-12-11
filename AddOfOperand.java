605781f328860a6445892607419bedfeed476eda
mahalaxmi571 <75772427+mahalaxmi571@users.noreply.github.com>
2020-12-11 20:23:54 +0530

    AddOfOperand

---
+++ AddOfOperand.java
@@ -0,0 +1,9 @@
+class AddOfOperand{
+     public static void main(String args[]){
+	       int a=1;
+		   int b=2;
+		   int c=3;
+		   int sum=a+b+c;
+	    System.out.println("the value of Sum is:"+sum);
+		 }
+		 }
